99d6b556317d60db5eb9da04ea55a26b9b164961
heo heo <mc@gmail.com>
2025-10-21 15:38:58 +0700

    QV-180: Set up Development Environments and QV-181: Build Data Model and Database

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -2,21 +2,32 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 
 @Module({
   imports: [
-    TypeOrmModule.forRoot({
-      type: 'mysql',
-      host: 'localhost',
-      port: 3306,
-      username: 'root',
-      password: 'Chi@261189',
-      database: 'QuizVerse',
-      autoLoadEntities: true,
-      synchronize: true, // chỉ dùng khi dev
+    ConfigModule.forRoot({
+      isGlobal: true,      
+      envFilePath: '.env', 
+    }),
+
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],  
+      useFactory: (configService: ConfigService) => ({
+        type: 'mysql',
+        host: configService.get<string>('DB_HOST'),
+        port: configService.get<number>('DB_PORT'),
+        username: configService.get<string>('DB_USERNAME'),
+        password: configService.get<string>('DB_PASSWORD'),
+        database: configService.get<string>('DB_DATABASE'),
+        autoLoadEntities: true,
+        synchronize: true, 
+      }),
     }),
   ],
   controllers: [AppController],
   providers: [AppService],
 })
+
 export class AppModule {}
